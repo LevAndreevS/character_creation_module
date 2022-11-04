83020cc05323634c046be0d849d600f046b214b6
Лев Андреев <leoohard@yandex.ru>
2022-11-04 23:15:06 +0300

    main.py -  the code is given in accordance with PEP257.

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-# character_creation_module/main.py
-# Новый импорт.
-# Из модуля start_game_banner, который расположен в папке graphic_arts,
-# импортируем функцию run_screensaver().
-from graphic_arts.start_game_banner import run_screensaver
+"""Документация модуля. Описывает работу классов и функций.
+"""
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Описывает работу функции attack."""
     ...
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Описывает работу функции defence."""
     ...
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Описывает работу функции special."""
     ...
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Описывает работу функции start_training."""
     ...
 
 
 def choice_char_class() -> str:
+    """Описывает работу функции выбор класса."""
     ...
-
-
-if __name__ == '__main__':
-    run_screensaver()
-    print('Приветствую тебя, искатель приключений!')
-    print('Прежде чем начать игру...')
-    char_name: str = input('...назови себя: ')
-    print(f'Здравствуй, {char_name}! '
-          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
-    print('Ты можешь выбрать один из трёх путей силы:')
-    print('Воитель, Маг, Лекарь')
-    char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
